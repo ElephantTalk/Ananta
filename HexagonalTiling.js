5c8eee313e8a44225989c7146013b9ef33827ac5
“ElephantTalk” <ibrahim.ali00@protonmail.com>
2021-10-28 10:12:22 -0700

    final

--- HexagonalTiling.js
+++ HexagonalTiling.js
@@ -4,10 +4,8 @@ function HexagonalTiling(r) {
   this.buildCell = function (x, y) {
     var sides = 6;
     var p = new Polygon(sides);
-    // rotate 360 degrees around the clock in 60 degree increments
     var inc = (2 * Math.PI) / sides;
     for (var index = 0; index < sides; index++) {
-      // angular to cartesian
       var θ = index * inc - inc / 2;
       var vX = x + r * Math.cos(θ);
       var vY = y + r * Math.sin(θ);
@@ -21,7 +19,6 @@ function HexagonalTiling(r) {
     var h = r * 2;
     var w = (sqrt(3) / 2) * h;
     var inc = 3 * (h / 4);
-    // TODO: get dimensions
     var row = 0;
     for (var y = -h / 2; y < height + h / 2; y += inc) {
       var startX = row % 2 == 0 ? -w : -w / 2;
